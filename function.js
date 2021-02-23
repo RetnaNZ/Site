15080ba2b67954a007439e4ac14bf5eca1350db4
RetnaNZ <71354492+RetnaNZ@users.noreply.github.com>
2021-02-23 23:40:17 +1300

    Big time switchover
    Making my Website now to display my school blogs

--- function.js
+++ function.js
@@ -4,7 +4,7 @@ const searchButton = document.getElementById('submitbutton');
 let clicked = 0;
 
 // searchInput.addEventListener('click', function(){
-    
+
 //     if(clicked === 0){
 //         searchInput.value = '' ;
 //         console.log("YOU CLICKED ME");
@@ -13,20 +13,21 @@ let clicked = 0;
 //     else{
 //         console.log(clicked);
 //     }
-        
+
 // });
 
 searchButton.addEventListener('click', function(){
-    
-    if(searchInput.value == 'Whananaki'){
-        document.open('.\\Whananki_Lodge\\index.html','', 'noopener=true')
-    }else if(searchInput.value == 'js'){
-        document.open('.\\js-playground\\index.html','', 'noopener=true')
-    }else if(searchInput.value == 'exp'){
-        document.open('.\\Expense Tracker\\index.html','', 'noopener=true')
-    }else if(searchInput.value == 'scroll'){
-        document.open('.\\Scrolling\\index.html','', 'noopener=true')
-    }else if(searchInput.value == 'expand'){
-        document.open('.\\Expand\\index.html','', 'noopener=true')
+
+    if(searchInput.value == 'BSA626'){
+        document.open('.\\zSites\\BSA626\\BSA626.html','', 'noopener=true')
+    }
+
+    if(searchInput.value == "BSA726"){
+        document.open('.\\zSites\\BSA726\\BSA726.html','', 'noopener=true')
+
+    }
+
+    if(searchInput.value == "BVA615"){
+        document.open('.\\zSites\\BVA615\\BVA615.html','', 'noopener=true')
     }
 });
