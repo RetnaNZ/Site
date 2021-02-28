3f2c89f2f39e00ed0d4a561da21e42b403f51693
RetnaNZ <Nat_pennington@hotmail.co.nz>
2021-03-01 12:53:34 +1300

    123

--- zSites/BSA726/app.js
+++ zSites/BSA726/app.js
@@ -1,8 +1,1 @@
 // JavaScript Document
-var textToBaloon;
-
-document.getElementsByClassName("BaloonText").innerHTML = textToBaloon;
-
-for	(var i = 0; i < textToBaloon.length; i++){
-	alert(textToBaloon[i]);
-}
