24caa0c947948dc928859ada7364f9ffb7725a22
RetnaNZ <Nat_pennington@hotmail.co.nz>
2021-03-01 10:39:20 +1300

    Pushing storyboard panels
    This is the panels from the storyboard

---
+++ zSites/BSA726/app.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+var textToBaloon;
+
+document.getElementsByClassName("BaloonText").innerHTML = textToBaloon;
+
+for	(var i = 0; i < textToBaloon.length; i++){
+	alert(textToBaloon[i]);
+}
