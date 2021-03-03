a516fd58fbed7d00c697236cbfc819abe290e9af
RetnaNZ <71354492+RetnaNZ@users.noreply.github.com>
2021-03-03 22:00:28 +1300

    Drawing Class
    This is the drawing classes website

--- zSites/BSA726/app.js
+++ zSites/BSA726/app.js
@@ -5,7 +5,7 @@ var backgroundIMG = document.getElementById('landing');
 
 var currentBackground = 0;
 
-let listOfBackgrounds = ["url(./images/HDRI.jpg)","url(./images/furballFloating.png)","url(./images/SIT_Logo.png)"];
+let listOfBackgrounds = ["url(./images/HDRI.jpg)","url(./images/furballFloating.png)","url(./images/furballFloating2.png)"];
 
 function moveBack(){
     
