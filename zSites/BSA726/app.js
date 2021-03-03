3ae4ec46c9bc87b08643111518794e42b5a504ad
RetnaNZ <71354492+RetnaNZ@users.noreply.github.com>
2021-03-03 20:49:57 +1300

    Update to the Advanced Animation Page
    Did a bit

--- zSites/BSA726/app.js
+++ zSites/BSA726/app.js
@@ -1,1 +1,37 @@
 // JavaScript Document
+
+var backgroundIMG = document.getElementById('landing');
+
+
+var currentBackground = 0;
+
+let listOfBackgrounds = ["url(./images/HDRI.jpg)","url(./images/furballFloating.png)","url(./images/SIT_Logo.png)"];
+
+function moveBack(){
+    
+    currentBackground -= 1;
+    if (currentBackground < 0){
+        currentBackground = listOfBackgrounds.length -1;
+    }
+    backgroundIMG.style.backgroundImage = listOfBackgrounds[currentBackground];
+    
+    
+
+
+console.log(currentBackground);
+
+}
+
+function moveForward(){
+
+    currentBackground += 1;
+    if (currentBackground > listOfBackgrounds.length -1 ){
+        currentBackground = 0
+    }
+
+    backgroundIMG.style.backgroundImage = listOfBackgrounds[currentBackground];
+    
+
+console.log(currentBackground);
+
+}
