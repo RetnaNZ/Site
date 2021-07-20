00638c37bd9731f3139dc0e8815d97c496f8da24
RetnaNZ <Nat_pennington@hotmail.co.nz>
2021-07-20 12:40:12 +1200

    Update
    Pushing the latest update

--- about/function.js
+++ about/function.js
@@ -5,6 +5,15 @@ const menuButton_contact = document.getElementById('contact');
 
 const iconButton = document.getElementById('icon_home');
 
+const member1 = document.getElementById('member_1');
+const member2 = document.getElementById('member_2');
+const member3 = document.getElementById('member_3');
+
+const member1_info = document.getElementById('member1_info');
+const member2_info = document.getElementById('member2_info');
+const member3_info = document.getElementById('member3_info');
+
+
 iconButton.onclick = function() {
 	location.href = "../index.html";
 }
@@ -24,3 +33,28 @@ menuButton_games.onclick = function() {
 menuButton_contact.onclick = function() {
 	location.href = "../contact/contactindex.html";
 }
+
+member1.addEventListener("mouseover", function(){
+	member1_info.style.display = "block";
+});
+
+member1.addEventListener("mouseleave", function(){
+	member1_info.style.display = "none";
+});
+
+member2.addEventListener("mouseover", function(){
+	member2_info.style.display = "block";
+});
+
+member2.addEventListener("mouseleave", function(){
+	member2_info.style.display = "none";
+});
+
+member3.addEventListener("mouseover", function(){
+	member3_info.style.display = "block";
+});
+
+member3.addEventListener("mouseleave", function(){
+	member3_info.style.display = "none";
+});
+
